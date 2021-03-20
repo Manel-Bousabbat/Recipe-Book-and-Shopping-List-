51b3fcd75db68f8792b22d2ed581e63757db8984
Manel-Bousabbat <manelbousabbat@gmail.com>
2021-03-20 10:02:59 +0100

    Adding Ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,31 @@
 
 import {EventEmitter} from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient} from '../shared/ingredient.model';
+
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe' , 'this is simply a test' , 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=640%2C360&ssl=1'),
-    new Recipe('Another Recipe' , 'this is another a test' , 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=640%2C360&ssl=1')];
+    new Recipe(
+      'Tasty Schnitzel' ,
+      'this is simply a test' ,
+      'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=640%2C360&ssl=1',
+    [
+      new Ingredient('Meat', 1 ) ,
+      new Ingredient('French Fries', 20 )
+    ]
+  ),
+    new Recipe(
+      'Another Recipe' ,
+  'this is another a test' ,
+  'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=640%2C360&ssl=1',
+
+  [
+    new Ingredient('Buns', 2),
+  new Ingredient('Meat', 1)
+]),
+
+];
 
   // tslint:disable-next-line:typedef
   getRecipes(){

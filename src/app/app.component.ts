cd12f229c12e368e0ed2a31f7e986daef0270579
Manel-Bousabbat <manelbousabbat@gmail.com>
2021-03-23 19:01:56 +0100

    Improving the Reactive Service with Observables (Subjects)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipe';
-  // tslint:disable-next-line:typedef
-  onNavigate(feature: string ) {
-    this.loadedFeature = feature ;
-  }
+
 }

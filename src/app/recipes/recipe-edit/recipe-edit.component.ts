25372d914e5a8df8e903902654f7865060df41be
Manel-Bousabbat <manelbousabbat@gmail.com>
2021-04-09 15:31:39 +0100

    Adding Ingredient Controls to a Form Array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute , Params} from '@angular/router';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
 import {RecipeService} from '../recipe.service';
 
 @Component({
@@ -28,21 +28,46 @@ export class RecipeEditComponent implements OnInit {
   onSubmit(){
     console.log(this.recipeForm);
   }
+  // tslint:disable-next-line:typedef
+  onAddIngredient(){
+    ( this.recipeForm.get('ingredients') as FormArray).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl()
+      })
+    );
+  }
 // tslint:disable-next-line:typedef
 private initForm(){
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '' ;
+    const recipeIngredients = new FormArray([]);
     if (this.editMode){
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe.ingredients){
+        for (const ingredient of recipe.ingredients){
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount : new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       name : new FormControl(recipeName),
       imagePath : new FormControl(recipeImagePath),
-      description: new FormControl (recipeDescription)
+      description: new FormControl (recipeDescription),
+      ingredients: recipeIngredients
     });
 }
+  // tslint:disable-next-line:typedef
+  get controls() { // a getter!
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
 }
